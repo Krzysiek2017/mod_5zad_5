cf688dec2c11852650ff386b0046db4735cae23d
Krzysztof <musashimiyamoto@interia.eu>
2018-02-09 20:19:42 +0100

    mod_5zad_5

---
+++ config.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+	'GOOGLE_CLIENT_ID': '849730148197-il9enngbhrfgfeem6vtb0aktmin1ac2e.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'K59BOe17EmYfIb--1fPrG9FN',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
